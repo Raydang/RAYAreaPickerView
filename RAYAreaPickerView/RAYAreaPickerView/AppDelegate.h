772d627d1558df21a13fa3be4676e4c4df2e22d3
raydang <17151328@qq.com>
2015-05-25 18:02:41 +0800

    create project

---
+++ RAYAreaPickerView/RAYAreaPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAYAreaPickerView
+//
+//  Created by richerpay on 15/5/25.
+//  Copyright (c) 2015年 richerpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAYAreaPickerView/RAYAreaPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAYAreaPickerView
+//
+//  Created by richerpay on 15/5/25.
+//  Copyright (c) 2015年 richerpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
